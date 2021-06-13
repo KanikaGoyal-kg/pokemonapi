033b42787d0195baf9e163e45dc8b45537b44740
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-13 14:57:32 +0530

    doing pokemon api

--- src/CompA.js
+++ src/CompA.js
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const CompA = () => {
 
     const [num, setNum] = useState();
+    const [name, setName] = useState();
+    const [moves, setMove] = useState();
+
+    const API = async () => {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+        console.log(response.data.name);
+        setName(response.data.name)
+        setMove(response.data.moves.length)
+
+    }
+
+    useEffect(() => {
+        API();
+    },[])
 
     const selectedNumber = (event) => {
-        setNum(event.target.value)
+        setNum(event.target.value);
     }
 
     return (
 
         <>
-        <h1>You choose {num} value</h1>
+        <h1>You choose <span style={{color:'red'}}> {num} value </span></h1>
+        <h1>My name is <span style={{color:'red'}}> {name} name </span></h1>
+        <h1>I have <span style={{color:'red'}}> {moves} moves </span></h1>
         <select value={num} onChange={selectedNumber}>
             <option value="1">1</option>
             <option value="25">25</option>
